e620325ba6741f6f0362fa955ef975e066304fa3
deepak <deepak@excellencetechnologies.in>
2020-10-21 12:34:43 +0530

    assignment3

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="stylediv">
+  <button type="button" class="btn1">Buy Now</button>
+ <button type="button" class="btn2">Larger Button to Test what we did it correctly</button>
+</div>
+</body>
+</html>
